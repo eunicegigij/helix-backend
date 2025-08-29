efc9adc6e572b9347da2285e142be55ca6a3aeee
AdaIsaacEme <nkechieme.ada@gmail.com>
2025-08-29 11:48:44 +0000

    add missing Express app initialization, server log, and formatting cleanup

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-const connectDB=require("./database/database");
-connectDB();
 const express=require("express");
-app.listen(3000);
+const cors=require("cors");
+const connectDB=require("./database/database");
+
+const app =express();
+const PORT= process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+connectDB()
+    .then(()=>{
+        app.listen(3000, ()=>{
+            console.log("Server is running on http://localhost:${PORT}");
+        });
+    })
+    .catch((err)=>{
+        console.log(err,"Database Connection Failed");
+        process.exit(1);
+    });
+
+
+
+
+
