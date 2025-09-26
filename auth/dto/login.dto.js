7a7e9ba2f30deec4e4557e1bcb222dc039a133aa
AdaIsaacEme <nkechieme.ada@gmail.com>
2025-09-26 18:25:38 +0000

    corrected login dto

--- auth/dto/login.dto.js
+++ auth/dto/login.dto.js
@@ -1,7 +1,7 @@
 const { z } = require("zod");
 
 const loginBodySchema = z.object({
-  email: z.string().email("Please enter a valid email"),
+  email: z.email("Please enter a valid email"),
   password: z.string().min(1, "Password is required"),
 });
 
