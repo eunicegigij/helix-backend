f08a1a467ba1ea6b82ef974114ad26574ea6bd08
Eunice Jacob <jacobeunice40@gmail.com>
2025-10-12 20:07:52 +0100

    updated routes and added api prefix

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express=require("express");
-const cors=require("cors");
-const routeManager=require("./routes/routeManager");
+const express = require("express");
+const cors = require("cors");
+const routeManager = require("./routes/routeManager");
 
-const app =express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/",routeManager);
-app.use(express.urlencoded({extended:true}));
+app.get("/health", (req, res) => {
+  res.json({
+    message: "welcome to helix, server is up!",
+  });
+});
+app.use("/api", routeManager);
+app.use(express.urlencoded({ extended: true }));
 
-module.exports={app};
-
-
-
-
-
-
+module.exports = { app };
